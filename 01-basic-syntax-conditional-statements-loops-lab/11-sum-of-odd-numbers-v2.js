4e606a0b0506f521c34f05ac6e091d798b3b3f81
Yoanna Popova <yoanna.dp@gmail.com>
2023-01-16 21:13:21 +0200

    Add second solution to eleventh task.

---
+++ 01-basic-syntax-conditional-statements-loops-lab/11-sum-of-odd-numbers-v2.js
@@ -0,0 +1,12 @@
+function sumOfOddNumbers(count) {
+    let numForPrint = 1;
+    let sum = 0;
+
+    for (let i = 0; i < count; i++) {
+        console.log(numForPrint);
+        sum += numForPrint;
+        numForPrint += 2;
+    }
+
+    console.log(`Sum: ${sum}`);
+}

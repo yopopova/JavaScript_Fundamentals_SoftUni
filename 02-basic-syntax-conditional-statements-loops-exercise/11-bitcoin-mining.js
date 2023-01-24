7972679394ee9068ce4f1217fb1a9eafdcc10e90
Yoanna Popova <yoanna.dp@gmail.com>
2023-01-24 19:03:01 +0200

    Add solution to eleventh task.

---
+++ 02-basic-syntax-conditional-statements-loops-exercise/11-bitcoin-mining.js
@@ -0,0 +1,49 @@
+function bitcoinMining(arr) {
+    let arrL = arr.length;
+
+    let dayCounter = 0;
+    let goldGrams = 0;
+
+    let dayMoney = 0;
+    let totalAmount = 0;
+
+    let bitcoinsCounter = 0;
+    let firstDayBuyBitcoins = 0;
+    let purchasedBitcoins = 0;
+    let allBitcoins = 0;
+    const bitcoinPrice = 11949.16;
+
+    for(let i = 0; i < arrL; i++) {
+        dayCounter++;
+        goldGrams = arr[i];
+
+        if(dayCounter % 3 === 0) {
+            goldGrams -= goldGrams * 0.3;
+        } else {
+            goldGrams = goldGrams;
+        }
+
+        dayMoney = goldGrams * 67.51;
+        totalAmount += dayMoney;
+
+        if(totalAmount >= bitcoinPrice) {
+            bitcoinsCounter++;
+
+            if(bitcoinsCounter === 1) {
+                firstDayBuyBitcoins = dayCounter;
+            }
+
+            purchasedBitcoins = Math.floor(totalAmount / bitcoinPrice);
+            allBitcoins += purchasedBitcoins;
+            totalAmount -= bitcoinPrice * purchasedBitcoins;
+        }
+    }
+
+    console.log(`Bought bitcoins: ${allBitcoins}`);
+
+    if(bitcoinsCounter > 0) {
+        console.log(`Day of the first purchased bitcoin: ${firstDayBuyBitcoins}`);
+    }
+
+    console.log(`Left money: ${totalAmount.toFixed(2)} lv.`);
+}

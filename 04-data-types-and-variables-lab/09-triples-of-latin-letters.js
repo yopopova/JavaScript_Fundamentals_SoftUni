f85e74892922e2f4ec9d41a73eb43079aa9deee4
Yoanna Popova <yoanna.dp@gmail.com>
2023-02-07 09:51:16 +0200

    Add solution to ninth task.

---
+++ 04-data-types-and-variables-lab/09-triples-of-latin-letters.js
@@ -0,0 +1,17 @@
+function triplesOfLatinLetters(num) {
+    num = Number(num);
+    let start = 97;
+    let end = start + num;
+
+    for (let i = start; i < end; i++) {
+        for (let k = start; k < end; k++) {
+            for (let m = start; m < end; m++) {
+                let firstLetter = String.fromCharCode(i);
+                let secondLetter = String.fromCharCode(k);
+                let thirdLetter = String.fromCharCode(m);
+
+                console.log(`${firstLetter}${secondLetter}${thirdLetter}`);
+            }
+        }
+    }
+}

a1b5f365c08a0c82e7130662b7fb310737a41532
Yoanna Popova <yoanna.dp@gmail.com>
2023-02-07 09:45:17 +0200

    Add third solution to eighth task.

---
+++ 04-data-types-and-variables-lab/08-special-numbers-v3.js
@@ -0,0 +1,21 @@
+function specialNumbers(num) {
+    for (let i = 1; i <= num; i++) {
+        let sum = 0;
+        let tempNum = i;
+
+        while (tempNum > 0) {
+            sum += tempNum % 10;
+            tempNum = parseInt(tempNum / 10);
+        }
+
+        let isSpecial;
+
+        if (sum === 5 || sum === 7 || sum === 11) {
+            isSpecial = "True";
+        } else {
+            isSpecial = "False";
+        }
+
+        console.log(`${i} -> ${isSpecial}`);
+    }
+}

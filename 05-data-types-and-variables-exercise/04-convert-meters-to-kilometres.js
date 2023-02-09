30f54989fdc41b3ad2f9a108401acac45565888c
Yoanna Popova <yoanna.dp@gmail.com>
2023-02-09 10:40:32 +0200

    Add solution to fourth task.

---
+++ 05-data-types-and-variables-exercise/04-convert-meters-to-kilometres.js
@@ -0,0 +1,5 @@
+function convertMetersToKilometres(input) {
+    let distanceInMeters = Number(input);
+    let distanceInKilometers = distanceInMeters / 1000;
+    console.log(distanceInKilometers.toFixed(2));
+}

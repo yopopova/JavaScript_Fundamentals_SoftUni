a10d6fd2941587bb7d90b37711fe4543b317d252
Yoanna Popova <yoanna.dp@gmail.com>
2023-02-10 10:57:54 +0200

    Add second solution to seventh task.

---
+++ 05-data-types-and-variables-exercise/07-lower-or-upper-v2.js
@@ -0,0 +1,13 @@
+function lowerOrUpper(input) {
+    let char = input.toString();
+    let upper = char.toUpperCase();
+    let result = '';
+
+    if (upper === char) {
+        result = 'upper-case';
+    } else {
+        result = 'lower-case';
+    }
+
+    console.log(result);
+}

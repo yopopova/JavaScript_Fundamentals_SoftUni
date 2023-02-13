7437abdcff6f7f56aa80cff69cadb79d18fbdfc6
Yoanna Popova <yoanna.dp@gmail.com>
2023-02-13 09:37:25 +0200

    Add solution to eighth tast.

---
+++ 05-data-types-and-variables-exercise/08-calculator.js
@@ -0,0 +1,18 @@
+function calculator(fNum, oper, sNum) {
+    let firstNum = Number(fNum);
+    let operator = oper.toString();
+    let secondNum = Number(sNum);
+    let result = 0;
+
+    if (operator === '*') {
+        result = firstNum * secondNum;
+    } else if (operator === '/') {
+        result = firstNum / secondNum;
+    } else if (operator === '+') {
+        result = firstNum + secondNum;
+    } else if (operator === '-') {
+        result = Math.abs(firstNum - secondNum);
+    }
+
+    console.log(result.toFixed(2));
+}

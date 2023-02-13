2904114f7e8a763126dd0de2f4a439e1b4d40e1d
Yoanna Popova <yoanna.dp@gmail.com>
2023-02-13 09:40:12 +0200

    Add second solution to eighth task.

---
+++ 05-data-types-and-variables-exercise/08-calculator-v2.js
@@ -0,0 +1,15 @@
+function calculator(fNum, oper, sNum) {
+    let firstNum = Number(fNum);
+    let operator = oper.toString();
+    let secondNum = Number(sNum);
+    let result = 0;
+
+    switch (operator) {
+        case '+': result = firstNum + secondNum; break;
+        case '-': result = Math.abs(firstNum - secondNum); break;
+        case '*': result = firstNum * secondNum; break;
+        case '/': result = firstNum / secondNum; break;
+    }
+
+    console.log(result.toFixed(2));
+}

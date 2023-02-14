bf8e790cc31382a622b7c9ac00f9c73b25e920d8
Yoanna Popova <yoanna.dp@gmail.com>
2023-02-14 09:53:36 +0200

    Add solution to third task.

---
+++ 06-data-types-and-variables-extra-tasks/03-cone.js
@@ -0,0 +1,12 @@
+function cone(r, h) {
+    r = Number(r);
+    h = Number(h);
+
+    let volume = 1 / 3 * Math.PI * Math.pow(r, 2) * h;
+
+    let s = Math.sqrt(Math.pow(r, 2) + Math.pow(h, 2));
+    let sOne = Math.PI * r * (r + s);
+
+    console.log(`volume = ${volume.toFixed(4)}`);
+    console.log(`area = ${sOne.toFixed(4)}`);
+}

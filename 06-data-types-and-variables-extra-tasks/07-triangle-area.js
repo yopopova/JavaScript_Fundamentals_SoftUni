41414051f012cd57aa426a9c93c2143e224c3f38
Yoanna Popova <yoanna.dp@gmail.com>
2023-02-15 09:35:09 +0200

    Add solution to seventh task.

---
+++ 06-data-types-and-variables-extra-tasks/07-triangle-area.js
@@ -0,0 +1,9 @@
+function triangleArea(a, b, c) {
+    a = Number(a);
+    b = Number(b);
+    c = Number(c);
+
+    let p = (a + b + c) / 2;
+    let s = Math.sqrt(p * (p - a) * (p - b) * (p - c));
+    console.log(s);
+}

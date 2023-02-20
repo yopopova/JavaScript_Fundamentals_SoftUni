68c0e7ce0d7ade930b0557901d30130fc1951f50
Yoanna Popova <yoanna.dp@gmail.com>
2023-02-20 11:48:12 +0200

    Add third solution to fourth task.

---
+++ 08-arrays-lab/04-reverse-an-array-of-strings-v3.js
@@ -0,0 +1,10 @@
+function reverseAnArrayOfStrings(arr) {
+
+    for (let i = 0; i < arr.length / 2; i++) {
+        let buffer = arr[i];
+        arr[i] = arr[arr.length - 1 - i];
+        arr[arr.length - 1 - i] = buffer;
+    }
+
+    console.log(arr.join(' '));
+}

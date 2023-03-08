1b4acfbb3fe162c511e34f5bbfcb92730f8de800
Yoanna Popova <yoanna.dp@gmail.com>
2023-03-08 12:03:46 +0200

    Add solution to tenth task.

---
+++ 09-arrays-exercise/10-ladybugs.js
@@ -0,0 +1,45 @@
+function ladybugs(arr) {
+    let copyArr = arr.slice();
+    let fieldSize = copyArr.shift();
+    let ladybugPositions = copyArr.shift().split(' ');
+    let finalPositions = new Array(fieldSize).fill(0);
+
+    for (let position of ladybugPositions) {
+        let currPosition = Number(position);
+
+        if (currPosition < 0 || currPosition >= fieldSize) {
+            continue;
+        }
+
+        finalPositions[currPosition] = 1;
+    }
+
+    for (let line of copyArr) {
+        let [index, command, jumpLength] = line.split(' ');
+        index = Number(index);
+        jumpLength = Number(jumpLength);
+
+        if (index < 0 || index >= finalPositions.length || finalPositions[index] !== 1) {
+            continue;
+        }
+
+        finalPositions[index] = 0;
+
+        if (command === 'left') {
+            jumpLength = -jumpLength;
+        }
+
+        index += jumpLength;
+
+        while (index >= 0 && index < finalPositions.length) {
+            if (finalPositions[index] === 0) {
+                finalPositions[index] = 1;
+                break;
+            }
+
+            index += jumpLength;
+        }
+    }
+
+    console.log(finalPositions.join(' '));
+}

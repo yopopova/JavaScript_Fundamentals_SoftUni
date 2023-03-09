c0bd277ee2fcf4cafb29daf7bd3a2eeb4c180041
Yoanna Popova <yoanna.dp@gmail.com>
2023-03-09 07:36:01 +0200

    Add second solution to third task.

---
+++ 10-arrays-extra-tasks/03-rotate-array-v2.js
@@ -0,0 +1,11 @@
+function rotateArray(array) {
+    let arr = array.slice(0, array.length - 1);
+    let step = Number(array.slice().pop());
+
+    for (let i = 0; i < step; i++) {
+        let lastElement = arr.pop();
+        arr.unshift(lastElement);
+    }
+
+    console.log(arr.join(' '));
+}

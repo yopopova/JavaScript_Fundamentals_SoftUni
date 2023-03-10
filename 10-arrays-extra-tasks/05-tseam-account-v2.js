f1f5dd801b48b08263abd5a201fba28569daa310
Yoanna Popova <yoanna.dp@gmail.com>
2023-03-10 16:52:54 +0200

    Add second solution to fifth task.

---
+++ 10-arrays-extra-tasks/05-tseam-account-v2.js
@@ -0,0 +1,38 @@
+function tseamAccount(arr) {
+    let newAccount = arr[0].split(' ');
+    let arrL = arr.length;
+
+    for (let i = 1; i < arrL; i++) {
+        let text = arr[i].split(' ');
+        let command = text[0];
+        let game = text[1];
+
+        if (command === 'Install' && newAccount.includes(game) === false) {
+            newAccount.push(game);
+
+        } else if (command === 'Uninstall' && newAccount.includes(game)) {
+            let index = newAccount.indexOf(game);
+            newAccount.splice(index, 1);
+
+        } else if (command === 'Update' && newAccount.includes(game)) {
+            let index = newAccount.indexOf(game);
+            newAccount.splice(index, 1);
+            newAccount.push(game);
+
+        } else if (command === 'Expansion') {
+            let expansionGame = game.split('-');
+            let firstPart = expansionGame[0];
+
+            if (newAccount.includes(firstPart)) {
+                let newGame = expansionGame.join(':');
+                let index = newAccount.indexOf(firstPart);
+                newAccount.splice(index + 1, 0, newGame);
+            }
+
+        } else if (command === 'Play!') {
+            break;
+        }
+    }
+
+    console.log(newAccount.join(' '));
+}

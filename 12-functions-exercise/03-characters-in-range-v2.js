64ff135ab803e3c9ba65974e42edd3f1f781d34a
Yoanna Popova <yoanna.dp@gmail.com>
2023-03-23 14:37:43 +0200

    Add second solution to second task.

---
+++ 12-functions-exercise/03-characters-in-range-v2.js
@@ -0,0 +1,15 @@
+function charactersInRange(first, second) {
+    let firstSymbolAsNumber = first.charCodeAt(0);
+    let secondSymbolAsNumber = second.charCodeAt(0);
+
+    let smallerNum = Math.min(firstSymbolAsNumber, secondSymbolAsNumber);
+    let biggerNum = Math.max(firstSymbolAsNumber, secondSymbolAsNumber);
+    let result = [];
+
+    for (let i = smallerNum + 1; i < biggerNum; i++) {
+        let currentElement = String.fromCharCode(i);
+        result.push(currentElement);
+    }
+
+    console.log(result.join(' '));
+}

c926d89d9cbbe98fe343083d29c291195228cb36
Yoanna Popova <yoanna.dp@gmail.com>
2023-03-27 17:44:53 +0300

    Add second solution to seventh task.

---
+++ 12-functions-exercise/07-nxn-matrix-v2.js
@@ -0,0 +1,14 @@
+function matrix(n) {
+    function rows(num) {
+        let firstRow = (num + ' ').repeat(num);
+        return firstRow + '\n';
+    }
+
+    function columns(arg) {
+        for(let i = 1; i <= arg; i++) {
+            return rows(arg).repeat(arg);
+        }
+    }
+
+    return columns(n);
+}

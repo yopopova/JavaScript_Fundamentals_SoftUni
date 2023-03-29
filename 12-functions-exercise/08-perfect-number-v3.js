3be4d578cc9e6e5bedc813907a9315b7d298b8fa
Yoanna Popova <yoanna.dp@gmail.com>
2023-03-29 09:29:12 +0300

    Add third solution to eighth task.

---
+++ 12-functions-exercise/08-perfect-number-v3.js
@@ -0,0 +1,16 @@
+function perfectNumber(n) {
+
+    function perfectOrNot(num) {
+        let sum = 0;
+
+        for (let i = 1; i < num; i++) {
+            if (num % i === 0) {
+                sum += i;
+            }
+        }
+        return sum;
+    }
+
+    let result = perfectOrNot(n) === n ? "We have a perfect number!" : "It's not so perfect.";
+    console.log(result);
+}

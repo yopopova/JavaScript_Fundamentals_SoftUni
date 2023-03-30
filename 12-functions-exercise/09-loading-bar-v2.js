121182a8effc78c350ca8ec8e43f54268e70a459
Yoanna Popova <yoanna.dp@gmail.com>
2023-03-30 12:05:37 +0300

    Add second solution to ninth task.

---
+++ 12-functions-exercise/09-loading-bar-v2.js
@@ -0,0 +1,20 @@
+function loadingBar(n) {
+
+    function loadedItems(num) {
+        return '%'.repeat(num / 10);
+    }
+
+    function dots(num) {
+        return '.'.repeat((100 - num) / 10);
+    }
+
+    let result = '';
+
+    if (n === 100) {
+        result = `${n}% Complete!\n` + '[' + loadedItems(n) + ']';
+    } else {
+        result = `${n}% [${loadedItems(n)}${dots(n)}]\nStill loading...`;
+    }
+
+    console.log(result);
+}

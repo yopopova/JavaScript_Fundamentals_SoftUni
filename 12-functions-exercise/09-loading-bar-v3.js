d430713da6599c1c7291c28439b8a0de0e8b7929
Yoanna Popova <yoanna.dp@gmail.com>
2023-03-30 12:08:24 +0300

    Add third solution to ninth task.

---
+++ 12-functions-exercise/09-loading-bar-v3.js
@@ -0,0 +1,14 @@
+function loadingBar(n) {
+
+    function loadedItems(num) {
+        return '%'.repeat(num / 10);
+    }
+
+    function dots(num) {
+        return '.'.repeat((100 - num) / 10);
+    }
+
+    let result = n === 100 ? `${n}% Complete!\n` + '[' + loadedItems(n) + ']' : `${n}% [${loadedItems(n)}${dots(n)}]\nStill loading...`;
+
+    console.log(result);
+}

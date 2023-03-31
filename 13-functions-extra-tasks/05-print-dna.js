4d53aaf4c8cec174deaf8cef50b48c5be573f400
Yoanna Popova <yoanna.dp@gmail.com>
2023-03-31 11:07:40 +0300

    Add solution to fifth task.

---
+++ 13-functions-extra-tasks/05-print-dna.js
@@ -0,0 +1,19 @@
+function printDna(number) {
+    let helix = 'ATCGTTAGGG';
+    let counter = 0;
+
+    for (let i = 0; i < number; i++) {
+
+        if (i % 4 == 0) {
+            console.log(`**${helix[counter % 10]}${helix[(counter % 10) + 1]}**`);
+        } else if (i % 4 == 1) {
+            console.log(`*${helix[counter % 10]}--${helix[(counter % 10) + 1]}*`);
+        } else if (i % 4 == 2) {
+            console.log(`${helix[counter % 10]}----${helix[(counter % 10) + 1]}`);
+        } else if (i % 4 == 3) {
+            console.log(`*${helix[counter % 10]}--${helix[(counter % 10) + 1]}*`);
+        }
+
+        counter += 2;
+    }
+}

a5442ae8d110041acb2a1a946e0a02fffa9744c3
Yoanna Popova <yoanna.dp@gmail.com>
2023-03-31 11:01:03 +0300

    Add solution to first task.

---
+++ 13-functions-extra-tasks/01-car-wash.js
@@ -0,0 +1,24 @@
+function carWash(arrOfCommands) {
+    let cleaningState = 0;
+
+    function cleaning(arr) {
+        for (let i = 0; i < arr.length; i++) {
+            let element = arr[i];
+
+            if (element === 'soap') {
+                cleaningState += 10;
+            } else if (element === 'water') {
+                cleaningState += cleaningState * 0.2;
+            } else if (element === 'vacuum cleaner') {
+                cleaningState += cleaningState * 0.25;
+            } else if (element === 'mud') {
+                cleaningState -= cleaningState * 0.1;
+            }
+        }
+
+        return cleaningState;
+    }
+
+    let cleaningPercentage = cleaning(arrOfCommands);
+    console.log(`The car is ${cleaningPercentage.toFixed(2)}% clean.`);
+}

fa26042cc071f7c6bc8efa7d44bd56a4491ab28e
Yoanna Popova <yoanna.dp@gmail.com>
2023-04-12 08:37:05 +0300

    Add solution to seventh task.

---
+++ 14-arrays-advanced-lab/07-list-of-products.js
@@ -0,0 +1,12 @@
+function listOfProducts(input) {
+
+    let sortedInput = input.sort((a, b) => {
+        return a.localeCompare(b);
+    });
+
+    let arrL = sortedInput.length;
+
+    for (let i = 0; i < arrL; i++) {
+        console.log(`${i + 1}.${sortedInput[i]}`);
+    }
+}

e611749c2c10ed369340d0f31ea4310394571220
Yoanna Popova <yoanna.dp@gmail.com>
2023-04-20 09:35:34 +0300

    Add solution to ninth task.

---
+++ 15-arrays-advanced-exercise/09-gladiator-inventory.js
@@ -0,0 +1,36 @@
+function gladiatorInventory(input) {
+    let inventory = input.shift().split(' ');
+    let inputL = input.length;
+
+    for (let i = 0; i < inputL; i++) {
+        let [command, item] = input[i].split(' ');
+        let index = inventory.indexOf(item);
+
+        if (command === 'Buy') {
+            if (!inventory.includes(item)) {
+                inventory.push(item);
+            }
+
+        } else if (command === 'Trash') {
+            if (inventory.includes(item)) {
+                inventory.splice(index, 1);
+            }
+
+        } else if (command === 'Repair') {
+            if (inventory.includes(item)) {
+                let reparedItem = inventory.splice(index, 1);
+                inventory.push(...reparedItem);
+            }
+
+        } else if (command === 'Upgrade') {
+            let [equipment, upgrade] = item.split('-');
+
+            if (inventory.includes(equipment)) {
+                let indexEquipment = inventory.indexOf(equipment);
+                inventory.splice(indexEquipment + 1, 0, `${equipment}:${upgrade}`)
+            }
+        }
+    }
+
+    console.log(inventory.join(' '));
+}

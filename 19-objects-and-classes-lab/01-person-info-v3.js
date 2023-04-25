7c14e919dba3a47299b401e49be49ca0082bf3e8
Yoanna Popova <yoanna.dp@gmail.com>
2023-04-25 11:29:48 +0300

    Add third solution to first task.

---
+++ 19-objects-and-classes-lab/01-person-info-v3.js
@@ -0,0 +1,12 @@
+function personInfo(fName, lName, personAge) {
+    class personalInfo {
+        constructor(firstName, lastName, age) {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.age = age;
+        }
+    }
+
+    let person = new personalInfo(fName, lName, personAge);
+    return person;
+}

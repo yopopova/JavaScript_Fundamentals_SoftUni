99cef0ca7317a7f0cc51caa245c5722f8fb7d6e8
Yoanna Popova <yoanna.dp@gmail.com>
2023-04-27 12:01:37 +0300

    Add third solution to fifth task.

---
+++ 19-objects-and-classes-lab/05-cats-v3.js
@@ -0,0 +1,21 @@
+function catsCreator(array) {
+    class Cat {
+        constructor(name, age) {
+            this.name = name,
+            this.age = age,
+            this.meow = () => {
+                console.log(`${this.name}, age ${this.age} says Meow`);
+            }
+        }
+    }
+
+    let cats = [];
+
+    for (let currentString of array) {
+        let currentData = currentString.split(' ');
+        let currentCat = new Cat(currentData[0], currentData[1]);
+        cats.push(currentCat);
+    }
+
+    cats.map(c => c.meow());
+}

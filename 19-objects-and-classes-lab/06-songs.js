8f608e0b642af53cbac5085e9125377631685d2e
Yoanna Popova <yoanna.dp@gmail.com>
2023-04-28 10:03:31 +0300

    Add solution to sixth task.

---
+++ 19-objects-and-classes-lab/06-songs.js
@@ -0,0 +1,28 @@
+function songs(input) {
+    class Song {
+        constructor(playlist, name, time) {
+            this.playlist = playlist,
+            this.name = name,
+            this.time = time,
+            this.print = () => console.log(this.name)
+        }
+    }
+
+    let numberOfSongs = input.shift();
+    let desiredPlaylist = input.pop();
+    let arrSongs = [];
+
+    for (let i = 0; i < numberOfSongs; i++) {
+        let currentInput = input[i];
+        let currentArray = currentInput.split('_');
+        let currentSong = new Song(currentArray[0], currentArray[1], currentArray[2]);
+        arrSongs.push(currentSong);
+    }
+
+    if (desiredPlaylist === 'all') {
+        arrSongs.forEach(s => s.print());
+    } else {
+        let filteredSongs = arrSongs.filter(s => s.playlist === desiredPlaylist);
+        filteredSongs.forEach(s => s.print());
+    }
+}

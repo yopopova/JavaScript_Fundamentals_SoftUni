8f58b6bc9915c09eebad90ac86a3132ef5f4b072
Yoanna Popova <yoanna.dp@gmail.com>
2023-05-29 17:10:17 +0300

    Add second solution to third task.

---
+++ 20-objects-and-classes-exercise/03-store-provision-v2.js
@@ -0,0 +1,28 @@
+function storeProvision(currentStocks, forDelivery) {
+    let storeProducts = {};
+    let currentStocksL = currentStocks.length;
+    let forDeliveryL = forDelivery.length;
+
+    for (let i = 0; i < currentStocksL; i += 2) {
+        let product = currentStocks[i];
+        storeProducts[product] = Number(currentStocks[i + 1]);
+    }
+
+    for (let i = 0; i < forDeliveryL; i += 2) {
+        let product = forDelivery[i];
+
+        if (!storeProducts.hasOwnProperty(product)) {
+            storeProducts[product] = 0;
+        }
+
+        storeProducts[product] += Number(forDelivery[i + 1]);
+    }
+
+    let entries = Object.entries(storeProducts).sort((a, b) => {
+        return a[1] - b[1];
+    });
+
+    entries.forEach(el => {
+        console.log(el);
+    });
+}

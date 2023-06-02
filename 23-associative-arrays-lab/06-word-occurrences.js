963363af126ba6482f83563675f12748ae70e1b9
Yoanna Popova <yoanna.dp@gmail.com>
2023-06-02 09:33:51 +0300

    Add solution to sixth task.

---
+++ 23-associative-arrays-lab/06-word-occurrences.js
@@ -0,0 +1,19 @@
+function wordOccurrences(input) {
+    let words = new Map();
+
+    for (let word of input) {
+        if (words.has(word)) {
+            let currentCount = words.get(word);
+            words.set(word, currentCount + 1);
+        } else {
+            words.set(word, 1);
+        }
+    }
+
+    let sortedWords = Array.from(words.entries());
+    sortedWords.sort((a, b) => b[1] - a[1]);
+
+    for (let wordEntry of sortedWords) {
+        console.log(`${wordEntry[0]} -> ${wordEntry[1]} times`);
+    }
+}

dbbc9e2fc65a08641af082c98bf9a7f42d4120c0
Yoanna Popova <yoanna.dp@gmail.com>
2023-06-12 17:15:11 +0300

    Add solution to third task.

---
+++ 27-text-processing-exercise/03-extract-file.js
@@ -0,0 +1,11 @@
+function extractFile(text) {
+    let index = text.lastIndexOf('\\');
+
+    let fileName = text.substring(index + 1).split('.');
+
+    let extension = fileName.pop();
+    let template = fileName.join('.');
+
+    console.log(`File name: ${template}`);
+    console.log(`File extension: ${extension}`);
+}

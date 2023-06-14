dc1042cd1744d08262d60996005202d62af94836
Yoanna Popova <yoanna.dp@gmail.com>
2023-06-14 08:42:42 +0300

    Add solution to seventh task.

---
+++ 27-text-processing-exercise/07-cut-and-reverse.js
@@ -0,0 +1,14 @@
+function cutAndReverse(str) {
+    let firstPart = str.substring(0, str.length / 2)
+    .split('')
+    .reverse()
+    .join('');
+
+    let secondPart = str.substring(str.length / 2)
+    .split('')
+    .reverse()
+    .join('');
+
+    console.log(firstPart);
+    console.log(secondPart);
+}

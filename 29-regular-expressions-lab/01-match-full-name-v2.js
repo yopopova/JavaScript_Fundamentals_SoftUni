ea1854b59353fad5969b40ca1cdee7203f6bd4a4
Yoanna Popova <yoanna.dp@gmail.com>
2023-06-20 21:28:29 +0300

    Add second solution to first task.

---
+++ 29-regular-expressions-lab/01-match-full-name-v2.js
@@ -0,0 +1,12 @@
+function matchFullName(text) {
+    let regEx = /\b[A-Z][a-z]+ [A-Z][a-z]+\b/g;
+
+    let matches = [];
+    let validMatch;
+
+    while ((validMatch = regEx.exec(text)) !== null) {
+        matches.push(validMatch[0]);
+    }
+
+    console.log(matches.join(' '));
+}

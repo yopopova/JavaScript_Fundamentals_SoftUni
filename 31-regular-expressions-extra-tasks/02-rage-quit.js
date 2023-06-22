ac13fd1b1678b50241c0ade2f6561f07c9abce74
Yoanna Popova <yoanna.dp@gmail.com>
2023-06-22 10:33:29 +0300

    Add solution to second task.

---
+++ 31-regular-expressions-extra-tasks/02-rage-quit.js
@@ -0,0 +1,30 @@
+function rageQuit(str) {
+    let regEx = /(?<nondigits>[\D]+)(?<digits>[0-9]+)/g;
+
+    let validMatch;
+    let arr = [];
+
+    while (validMatch = regEx.exec(str)) {
+        arr.push(validMatch);
+    }
+
+    let symbolsCount = [];
+    let symbolsArray = [];
+
+    for (let line of arr) {
+        let symbols = line[1].toUpperCase();
+        let num = Number(line[2]);
+
+        for (let letter of symbols) {
+            if (!symbolsCount.includes(letter)) {
+                symbolsCount.push(letter);
+            }
+        }
+
+        let repeatedSymbols = symbols.repeat(num);
+        symbolsArray.push(repeatedSymbols);
+    }
+
+    console.log(`Unique symbols used: ${symbolsCount.length}`);
+    console.log(symbolsArray.join(''));
+}

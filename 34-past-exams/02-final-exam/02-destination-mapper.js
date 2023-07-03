257ace8c3031395ff43a8ae7cee9ef6561a0fbf2
Yoanna Popova <yoanna.dp@gmail.com>
2023-07-03 09:33:38 +0300

    Add solution to second task.

---
+++ 34-past-exams/02-final-exam/02-destination-mapper.js
@@ -0,0 +1,25 @@
+function destinationMapper(input) {
+    let text = input;
+    let regEx = /(=|\/)([A-Z][A-Za-z]{3,})\1/g;
+
+    let validMatch;
+    let destinations = [];
+
+    while (validMatch = regEx.exec(text)) {
+        destinations.push(validMatch);
+    }
+
+    let travelPoints = 0;
+    let destinationNames = [];
+
+    for (let des of destinations) {
+        let currentDes = des[2];
+        let desPoints = currentDes.length;
+
+        destinationNames.push(currentDes);
+        travelPoints += desPoints;
+    }
+
+    console.log(`Destinations: ${destinationNames.join(', ')}`);
+    console.log(`Travel Points: ${travelPoints}`);
+}

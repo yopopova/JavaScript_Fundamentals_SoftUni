221a99ae583c04570614f7464268c4f39a9e11ae
Yoanna Popova <yoanna.dp@gmail.com>
2023-07-07 07:39:09 +0300

    Add second solution to first task.

---
+++ 34-past-exams/04-final-exam/01-password-reset-v2.js
@@ -0,0 +1,47 @@
+function passwordReset(input) {
+    let pass = input.shift();
+    let doneCommand = input.pop();
+    let inputL = input.length;
+
+    for (let i = 0; i < inputL; i++) {
+        let [command, elOne, elTwo] = input[i].split(' ');
+
+        if (command === 'TakeOdd') {
+            let passArr = pass.split('');
+            let passArrL = passArr.length;
+            let newPass = [];
+
+            for (let k = 0; k < passArrL; k++) {
+                let char = passArr[k];
+
+                if (k % 2 !== 0) {
+                    newPass.push(char);
+                }
+            }
+
+            pass = newPass.join('');
+            console.log(pass);
+
+        } else if (command === 'Cut') {
+            let index = Number(elOne);
+            let l = Number(elTwo);
+
+            let searchedSubstr = pass.substr(index, l);
+            pass = pass.replace(searchedSubstr, '');
+            console.log(pass);
+
+        } else if (command === 'Substitute') {
+            let subStr = elOne;
+            let subst = elTwo;
+
+            if (pass.includes(subStr)) {
+                pass = pass.split(subStr).join(subst);
+                console.log(pass);
+            } else {
+                console.log('Nothing to replace!');
+            }
+        }
+    }
+
+    console.log(`Your password is: ${pass}`);
+}

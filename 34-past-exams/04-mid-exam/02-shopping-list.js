f7d7ebc8ab07bfe7fcbb1adc2875dfb7b63cf411
Yoanna Popova <yoanna.dp@gmail.com>
2023-07-10 16:28:23 +0300

    Add solution to second task.

---
+++ 34-past-exams/04-mid-exam/02-shopping-list.js
@@ -0,0 +1,43 @@
+function shoppingList(input) {
+    let list = input.shift().split('!');
+    let line = input.shift();
+
+    while (true) {
+
+        if (line === "Go Shopping!") {
+            break;
+        }
+
+        let [command, ...groceries] = line.split(' ');
+        let productOne = groceries[0];
+        let productTwo = groceries[1];
+
+        let index = list.indexOf(productOne);
+
+        if (command === 'Urgent') {
+            if (!list.includes(productOne)) {
+                list.unshift(productOne);
+            }
+
+        } else if (command === 'Unnecessary') {
+            if (list.includes(productOne)) {
+                list.splice(index, 1);
+            }
+
+        } else if (command === 'Correct') {
+            if (list.includes(productOne)) {
+                list.splice(index, 1, productTwo);
+            }
+
+        } else if (command === 'Rearrange') {
+            if (list.includes(productOne)) {
+                let removedProduct = list.splice(index, 1);
+                list.push(removedProduct[0]);
+            }
+        }
+
+        line = input.shift();
+    }
+
+    console.log(list.join(', '));
+}

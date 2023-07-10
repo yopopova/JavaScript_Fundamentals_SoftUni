5cc77622a6ce5ce9c5733a962fd912a386ee036e
Yoanna Popova <yoanna.dp@gmail.com>
2023-07-10 16:30:14 +0300

    Add solution to first task.

---
+++ 34-past-exams/05-final-exam/01-activation-keys.js
@@ -0,0 +1,50 @@
+function activationKeys(input) {
+    let activationKey = input.shift();
+    let generateCommand = input.pop();
+    let inputL = input.length;
+
+    for (let i = 0; i < inputL; i++) {
+        let [command, elOne, elTwo, elThree] = input[i].split('>>>');
+
+        if (command === 'Contains') {
+            let givenStr = elOne;
+
+            if (activationKey.includes(givenStr)) {
+                console.log(`${activationKey} contains ${givenStr}`);
+            } else {
+                console.log("Substring not found!");
+            }
+
+        } else if (command === 'Flip') {
+            let action = elOne;
+            let startIn = Number(elTwo);
+            let endIn = Number(elThree);
+
+            let startPart = activationKey.substring(0, startIn);
+            let midPart = activationKey.substring(startIn, endIn);
+            let lastPart = activationKey.substring(endIn);
+
+            if (action === 'Upper') {
+                midPart = midPart.toUpperCase();
+            } else if (action === 'Lower') {
+                midPart = midPart.toLowerCase();
+            }
+
+            let newKey = startPart.concat(midPart, lastPart);
+            activationKey = newKey;
+            console.log(activationKey);
+
+        } else if (command === 'Slice') {
+            let startIndex = Number(elOne);
+            let endIndex = Number(elTwo);
+
+            let firstPart = activationKey.substring(0, startIndex);
+            let secondPart = activationKey.substring(endIndex);
+            let newStr = firstPart.concat(secondPart);
+            activationKey = newStr;
+            console.log(activationKey);
+        }
+    }
+
+    console.log(`Your activation key is: ${activationKey}`);
+}
